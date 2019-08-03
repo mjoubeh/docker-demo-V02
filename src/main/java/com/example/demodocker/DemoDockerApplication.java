fde1e0b21627118118aa12d033037f2ed3ad8a68
Mohanad Joubeh <m.joubeh@yahoo.com>
2019-08-03 09:59:24 -0700

    x

--- src/main/java/com/example/demodocker/DemoDockerApplication.java
+++ src/main/java/com/example/demodocker/DemoDockerApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoDockerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Line 1");
 		SpringApplication.run(DemoDockerApplication.class, args);
+
+		System.out.println("line 2");
+		Thread.sleep(1000 * 60);
+		System.out.println("line 3 agter 1 min of sleeping");
 	}
 }
